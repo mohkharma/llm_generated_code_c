97d4c5d129e66169ea5467832d75f525720c8503
mohammed kharma <moh.kharma@gmail.com>
2024-08-19 02:28:23 +0300

    complete the errors fixing

--- source/output/MISTRAL_codestral-latest/c/Task0_MISTRAL_codestral-latest.c
+++ source/output/MISTRAL_codestral-latest/c/Task0_MISTRAL_codestral-latest.c
@@ -1,9 +1,35 @@
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <dirent.h>
+#include <string.h>
 
-int countPassengers(int stops[][2], size_t len) {
-    int total = 0;
-    for (size_t i = 0; i < len; i++) {
-        total += stops[i][0] - stops[i][1];
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+void* process_file(void* filename) {
+    pthread_mutex_lock(&lock);
+    printf("Processing file: %s\n", (char*)filename);
+    // Add your file processing logic here
+    pthread_mutex_unlock(&lock);
+    free(filename);
+    return NULL;
+}
+
+int main() {
+    DIR* dir;
+    struct dirent* ent;
+    if ((dir = opendir("shared_directory")) != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            pthread_t thread;
+            char* filename = malloc(strlen(ent->d_name) + 1);
+            strcpy(filename, ent->d_name);
+            pthread_create(&thread, NULL, process_file, filename);
+            pthread_detach(thread);
+        }
+        closedir(dir);
+    } else {
+        perror("");
+        return EXIT_FAILURE;
     }
-    return total;
+    return 0;
 }
